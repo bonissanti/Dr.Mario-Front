da8b9b50faadc0edebe90e42ca998965cc798a46
Bruno Rodrigues Onissanti <brunoonissanti@gmail.com>
2025-12-17 22:43:31 -0300

    refactor: added new variables

--- Dr.Mario-Remake/src/main.ts
+++ Dr.Mario-Remake/src/main.ts
@@ -1,15 +1,35 @@
 import type {IRouter} from "./shared/services/Router/IRouter.ts";
 import {RouterService} from "./shared/services/Router/RouterService.ts";
+import {ComponentLoader} from "./shared/services/ComponentLoader/ComponentLoader.ts";
+
+const element: HTMLElement | null = document.getElementById('app');
+const loader = new ComponentLoader(element!);
+const router = new RouterService(loader);
 
 const routes: IRouter[] = [
     { path: '/', component: '/pages/home.html', name: 'Home' },
     { path: '/about', component: '/pages/about.html', name: 'About' },
     { path: '/contact', component: '/pages/howtoplay.html', name: 'How to Play' },
+    { path: '/main-menu', component: '/pages/main-menu/@components/@main-menu-component/mainMenu.html', name: 'Play Now' },
     { path: '/error-404', component: '/pages/error/error404.html', name: 'Error 404' },
 ]
 
-const router = RouterService.getInstance('app');
 routes.forEach(route => router.addRoutes(route));
 
-await router.handleRoute();
-(globalThis as any).router = router;
+(globalThis.window as any).appRouter = router;
+
+document.addEventListener('click', (event) => {
+    const target = event.target as HTMLElement;
+    const link = target.closest('a[data-link]');
+
+    if (link)
+    {
+        event.preventDefault();
+        const href = link.getAttribute('href');
+
+        if (href)
+            router.navigate(href);
+    }
+})
+
+router.handleRoute(globalThis.window.location.pathname)
