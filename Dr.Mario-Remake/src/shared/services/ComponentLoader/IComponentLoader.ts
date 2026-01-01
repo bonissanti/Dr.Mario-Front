cf238bf03533dbaef41c47b7b699213ef50f07d6
Bruno Rodrigues Onissanti <brunoonissanti@gmail.com>
2026-01-01 11:41:03 -0300

    refactor: improved route preloading and caching mechanisms in ComponentLoader and Router, added related tests

--- Dr.Mario-Remake/src/shared/services/ComponentLoader/IComponentLoader.ts
+++ Dr.Mario-Remake/src/shared/services/ComponentLoader/IComponentLoader.ts
@@ -1,5 +1,4 @@
 export interface IComponentLoader {
     load(componentPath: string): Promise<void>;
     preloadRoute(componentPath: string): Promise<void>;
-    preloadRoutes(componentPaths: string[]): Promise<void>;
 }
