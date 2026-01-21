71ac8d42ec7102eac6e1c5bd75a74c5556f25010
Bruno Rodrigues Onissanti <brunoonissanti@gmail.com>
2026-01-20 23:08:40 -0300

    feat: integrate authService into routerService for route guarding

--- Dr.Mario-Remake/src/app/services/Router/RouterService.ts
+++ Dr.Mario-Remake/src/app/services/Router/RouterService.ts
@@ -1,14 +1,17 @@
 import type {IRouter} from "./IRouter.ts";
 import type {IComponentLoader} from "../ComponentLoader/IComponentLoader.ts";
+import type {IAuthService} from "../AuthService/IAuthService.ts";
 
 export class RouterService
 {
     public readonly routes: IRouter[] = [];
     private readonly componentLoader: IComponentLoader;
+    private readonly authService: IAuthService;
 
-    constructor(componentLoader: IComponentLoader)
+    constructor(componentLoader: IComponentLoader, authService: IAuthService)
     {
         this.componentLoader = componentLoader;
+        this.authService = authService;
         globalThis.addEventListener('popstate', () => this.handleRoute(globalThis.location.pathname));
     }
 
@@ -19,14 +22,26 @@ export class RouterService
 
     public async handleRoute(path: string): Promise<void>
     {
-        const route: IRouter | undefined = this.routes.find(route => route.path === path);
-
-        if (!route)
+        try
         {
-            await this.componentLoader.load('/error-404');
-            return;
+            const route: IRouter | undefined = this.routes.find(route => route.path === path);
+
+            if (!route)
+               return await this.componentLoader.load('/error-404');
+
+            else if (route?.guardRoute === false)
+                return await this.componentLoader.load(route.component);
+
+            else if (route?.guardRoute === true && await this.authService.checkAuthentication())
+                return await this.componentLoader.load(route.component);
+
+            return await this.componentLoader.load('/auth/login');
         }
-        await this.componentLoader.load(route.component);
+        catch (error)
+        {
+            console.error(error);
+            await this.componentLoader.load('/error-500');
+        }
     }
 
     public async preloadRoute(path: string): Promise<void>
--- Dr.Mario-Remake/src/main.ts
+++ Dr.Mario-Remake/src/main.ts
@@ -2,17 +2,23 @@ import type {IRouter} from "./shared/services/Router/IRouter.ts";
 import {RouterService} from "./shared/services/Router/RouterService.ts";
 import {ComponentLoader} from "./shared/services/ComponentLoader/ComponentLoader.ts";
 import '@awesome.me/webawesome/dist/styles/webawesome.css';
+import {AuthService} from "./app/services/AuthService/AuthService.ts";
+import {Countries} from "./app/stores/Countries/Countries.ts";
 
 const element: HTMLElement | null = document.getElementById('app');
 const loader = new ComponentLoader(element!);
-const router = new RouterService(loader);
+const authService = new AuthService();
+const router = new RouterService(loader, authService);
+const countries: Countries = Countries.getInstance();
 
 const routes: IRouter[] = [
-    { path: '/', component: '/pages/home.html', name: 'Home' },
-    { path: '/about', component: '/pages/about.html', name: 'About' },
-    { path: '/contact', component: '/pages/howtoplay.html', name: 'How to Play' },
-    { path: '/main-menu', component: '/pages/components/main-menu/main-menu.html', name: 'Play Now' },
-    { path: '/error-404', component: '/pages/error/error404.html', name: 'Error 404' },
+    { path: '/', component: '/pages/home.html', name: 'Home', guardRoute: false },
+    { path: '/about', component: '/pages/about.html', name: 'About', guardRoute: false },
+    { path: '/contact', component: '/pages/howtoplay.html', name: 'How to Play', guardRoute: false },
+    { path: '/main-menu', component: '/pages/components/main-menu/main-menu.html', name: 'Play Now', guardRoute: false }, //TODU: should be true
+    { path: '/login-signUp', component: '/pages/components/login-signUp/login-signUp.html', name: 'Login or SignUp', guardRoute: false },
+    { path: '/sign-up', component: '/pages/components/login-signUp/sign-up.html', name: 'Sign-up', guardRoute: false },
+    { path: '/error-404', component: '/pages/error/error404.html', name: 'Error 404', guardRoute: false },
 ]
 
 globalThis.addEventListener('load', () => {
@@ -49,4 +55,20 @@ document.addEventListener('click', (event) => {
     }
 })
 
+const countriesList = countries.CountryNames;
+customElements.whenDefined('wa-select').then(() => {
+    const countrySelect = document.querySelector('wa-select[name="country"]');
+
+    if (countrySelect)
+    {
+        for (const country of countriesList) {
+            const option = document.createElement('wa-option');
+            option.textContent = `${country.name}`;
+            countrySelect.appendChild(option);
+        }
+    }
+});
+
 router.handleRoute(globalThis.window.location.pathname)
+
+//TODU: when user clicks in crea
