998204df8c9d6e922ac361fada0d865699d0d464
Bruno Rodrigues Onissanti <brunoonissanti@gmail.com>
2026-02-18 21:17:04 -0300

    refactor: deleted deprecated event bus, create userDTO and UI components

--- Dr.Mario-Remake/src/shared/stores/EventBus/Interface/IEventBus.ts
+++
@@ -1,9 +0,0 @@
-import {ISubscription} from "./ISubscription.ts";
-import {EventHandler} from "../Type/EventHandler.ts";
-
-export interface IEventBus<T>
-{
-    publish(event: T, payload?: any): void;
-    subscribe(handler: EventHandler<T>): ISubscription;
-    unsubscribe(key: string): void;
-}
